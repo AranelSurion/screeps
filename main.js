49392a928b4164c6f6f005ca99e66e7834524911
Aranel Surion <aranel@aranelsurion.org>
2016-01-24 12:38:23 +0200

    retvalue returns string on success, now handled properly.

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ module.exports.loop = function () {
         /* if (creep.memory.role == "upgrader") Builders now work as Upgraders. */
         /*    upgrader(creep);
         } */
-        if (creep.ticksToLive < 50 && creep.memory.willbereplaced != 1){ /* use renew instead? */
+        if (creep.ticksToLive < 50 && creep.memory.willbereplaced != 1){ /* handle creep decay */ /* use renew instead? */
             cbody = [];
             for (var i in creep.body)
             {
@@ -25,7 +25,7 @@ module.exports.loop = function () {
             }
             console.log(cbody);
             var retvalue = Game.spawns.Spawn1.createCreep(cbody, null, {role: creep.memory.role});
-            if (retvalue == 0){
+            if (_.isString(retvalue)){
                 creep.memory.willbereplaced = 1;
             }else{
                 console.log("Couldn't replicate a new creep. DEBUG:" + retvalue + cbody + creep.memory.role)
@@ -33,5 +33,5 @@ module.exports.loop = function () {
         } 
 
     
-	
+    
 }}
