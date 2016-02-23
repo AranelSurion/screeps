745cb058b9d7fe6ee52b0fd405219dd9fef25632
Aranel Surion <aranel@aranelsurion.org>
2016-02-23 06:56:20 +0200

    completely rewritten

--- README.md
+++ README.md
@@ -1,23 +1,39 @@
 # ScreepsJS Bot
 
-ScreepsJS is my very simple bot/AI built on top of Screeps tutorial, for the JavaScript-based MMO Strategy game Screeps.com.
+ScreepsJS is my simple bot/AI built for the JavaScript-based MMO Strategy game Screeps.com.
 
-Nothing fancy, but it's fine for the first few hours of the game and (more-or-less) self-sufficient if enough number of Extensions are provided.
+Nothing fancy, but it's fine for up to RCL8 and (more-or-less) self-sufficient if enough number of Extensions are provided. 
+
 
 # Features
-Apart from the original tutorial features, this bot also can:
+This bot can:
+- Renew or recreate creeps.
+- Handle Walls, Ramparts, Towers, Links and Storage.
+- Defend rooms and notify you of hostile presence.
+- Provide many roles for different tasks.
+- Run with many rooms and spawns. 
 
-- Handle timeToLife of Creeps and renew them as needed.
-- Recreate creeps if renewal fails and they fall into decay.
-- Builders can also maintain Room Controller and work as an Upgrader. (there's also an Upgrader role, if you prefer)
-- Builders won't drain a Spawn under half of its capacity.
-- Custodian role. Maintains roads and structures under  %90 integrity, also fortifies walls and ramparts in its spare time. (ignores rookie walls)
-- Miner/Carrier roles to replace Harvester role. Also refills builders if upgrading Room Controller.
-- Guards can talk, and notify you of hostile presence. They can be ordered to move by using GUARD_MOVE flag.
-- Spawn guards automatically on hostile presence. You'll also receive a notification.
-- Harvesters use and prioritize Extensions, they can also switch back and forth.
 
-**Note**: "Spawn1" is the hardcoded spawn name.
+# Roles and Settings
+Settings can be changed by altering creep memory. Syntax: Memory.creeps.Creepname.setting=value
+
+- Builder: Builds structures and upgrades the controller.
+	- waitmode: If set 1, Builder will not return to fill itself for upgrading, it will wait for a Carrier or an adjacent Link to provide energy.
+- Miner: Stationary miner, harvests resource and hands it over to a Carrier.
+	- cstorage: If set 1, Miner will store energy in adjacent storage instead.
+
+- Carrier
+- Custodian
+- Guard
+- Harvester
+- Hauler
+- Claimer
+
+- Buildernew
+- Custodiannew
+
+- Upgrader
+
 
 # Installation
 
--- carrier.js
+++ carrier.js
@@ -3,112 +3,230 @@
  * module.exports = 'a thing';
  *
  * You can import it from another modules like this:
- * var mod = require('harvester'); // -> 'a thing'
+ * var mod = require('carrier'); // -> 'a thing'
  */
  
- module.exports = function (creep) {
-	if (creep.carry.energy == 0) { /* loading */
-	    creep.memory.fillmode = 1;
-	}
-	if (creep.carry.energy == 200){
-	    creep.memory.fillmode = 0;
-	}
-	
-	if (creep.memory.fillmode == 1){ /* IF STRUCTURE_STORAGE DOES NOT EXIST */
-	    creep.memory.passbit = 0;
-        upgradetarget = Game.getObjectById(creep.memory.upgradetarget); /* cleaning wait bits */
-        if (upgradetarget){upgradetarget.memory.waitbit = 0
-        creep.memory.upgradetarget = 0;}
-        
-        if (!creep.room.storage){
-        /* finding closest miner */
-        var miner = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
-        filter: function(object) {
-            return object.memory.role == "miner" && object.carry.energy > 10;
-        }
-        });
+module.exports = function (creep) {
     
-         if (miner && miner.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-             creep.moveTo(miner);
-         }else{
-             if(creep.pos.findClosestByRange(FIND_DROPPED_ENERGY)){
-             creep.pickup(creep.pos.findClosestByRange(FIND_DROPPED_ENERGY));
-             }
-         }}else{ /* STRUCTURE_STORAGE EXISTS */
-             if (creep.room.storage.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                 creep.moveTo(creep.room.storage);
-             }
-         }
-	    
-	} else { /* unloading */
-	    if (creep.memory.passbit == 1){
-	        /*console.log("debug: selected spawn");*/
-	    var transferYeri = Game.spawns.Spawn1;
-	    /* periodically check extensions - guess it's better than running find every iteration. */
-	    if (creep.memory.chkcount){
-	        creep.memory.chkcount = creep.memory.chkcount + 1;
-	    }else{
-	        creep.memory.chkcount = 1;
-	    }
-	    process = creep.transfer(transferYeri, RESOURCE_ENERGY);
-	    }
-	    
-	    if (creep.memory.passbit == 0){
-	        /*console.log("debug: selected extension");*/
-	    var transferYeri = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: function (s){
-	        return s.structureType == STRUCTURE_EXTENSION && s.energy < s.energyCapacity
-	    }
-	    });
-	    process = creep.transfer(transferYeri, RESOURCE_ENERGY);
-	    }
-		if( process == ERR_NOT_IN_RANGE) {
-			creep.moveTo(transferYeri);
-		}
-		if( process == ERR_FULL && transferYeri == Game.spawns.Spawn1 || creep.memory.passbit == 2) {
-            console.log("Carrier: Spawn is full.");
-            /* find an upgrade-mode builder */
-            
-            var upgrader = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
-            filter: function(object) {
-            return object.memory.role == "builder" && object.memory.upgrademode == 1 && object.carry.energy != object.carryCapacity;
+    // TODO - DEFCON ARRAYS
+    
+    /* VARIABLES */
+    var flagname = "Game.flags." + creep.name + "_MOVE";
+    var jobs = creep.memory.jobs // ARRAY of Jobs
+    var job = creep.memory.job; // Selected Job
+    var target = creep.memory.target;
+    
+    /* DEFAULT JOB ARRAY */
+    /* JOB CODES: -9: stop, -1: move, 0: fill, 1: extension, 2:spawn, 3:tower, 4:builders, 5: storage, 6: link */
+    if (jobs == null){
+        jobs = [0, 1, 2, 3, 4, 5, 6]; 
+        creep.memory.jobs = jobs;
+    }
+    
+    /* EVENT CODE */
+    function decideJob(){
+    //if (DEFCON == 1){ return -9;} // DEFCON 1, STOP.
+    if (job == null){return 0;} // default job
+    if (eval(flagname)){ return -1;} // MOVE ORDER received
+    if (!eval(flagname) && job == -1){ return 0;} // MOVE ORDER removed, go back to work.
+    if (creep.carry.energy == 0){ return 0;} // creep empty
+    //if (creep.carry.energy < creep.carryCapacity/4){ return 0;}// not worth it
+    return creep.memory.job;
+    }
+    
+    job = decideJob();
+    creep.memory.job = job;
+
+    /* hack to not allow array overflow */
+    if (job > jobs.length - 1){
+        creep.memory.job = creep.memory.job - (jobs.length - 1);
+        job = creep.memory.job;
+    }
+    
+    if (job < 0){
+        job = job;
+    }else{
+        job = jobs[job];
+    }
+    console.log(creep.name + ": Working on job# " + job);
+    
+    /* JOB DEFINITIONS */
+    
+    switch (job){
+        case -9: // STOP
+            break;
+        case -1: // MOVE ORDER
+            creep.cancelOrder("move"); // cancel ALL move orders 
+            creep.moveTo(eval(flagname));
+            break;   
+        
+        case 0: // FILL. FROM MINER OR STORAGE.
+        
+        
+            if (creep.carry.energy == creep.carryCapacity){
+                creep.memory.job = creep.memory.job + 1;
+                break;
+            }
+            if (creep.memory.target == "storage"){
+                if (creep.room.storage.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(creep.room.storage);
+                }
+            }else if (creep.memory.target == "spawn"){ // TODO IMPLEMENT THIS - MEMORY PREFFERED SPAWN
+                var spawnloc = creep.room.find(FIND_MY_SPAWNS)[0];
+                if (spawnloc != null){
+                    if (spawnloc.energy >= 50){
+                        if (spawnloc.transferEnergy(creep) == ERR_NOT_IN_RANGE){
+                            creep.moveTo(spawnloc);
+                        }
+                    }
+                }
+            }else{
+                /* finding closest miner */
+                var miner = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
+                filter: function(object) {
+                return object.memory.role == "miner" && object.memory.target == creep.memory.target && object.carry.energy > 10;
+                }
+                });     
+                
+                if (miner && miner.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(miner);
+                }else{
+                    if(creep.pos.findClosestByRange(FIND_DROPPED_ENERGY)){
+                    creep.pickup(creep.pos.findClosestByRange(FIND_DROPPED_ENERGY));
+                    }
+                }
+            }
+            break;
+        case 1: // EXTENSIONS
+            var extension = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: function (s){
+            return s.structureType == STRUCTURE_EXTENSION && s.energy < s.energyCapacity
             }
             });
             
-            if (upgrader){
-                /* console.log("debug: selected upgrader"); */
-                if (creep.carry.energy < 50){ /* not worth it, go back and refill */
-                    creep.memory.fillmode = 1;
-                }
-                creep.memory.passbit = 2;
-                creep.memory.upgradetarget = upgrader.id;
-                upgrader.memory.waitbit = 1;
-                if (creep.transfer(upgrader, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                    creep.moveTo(upgrader);
-                }
-                if (Game.spawns.Spawn1.energy < 150){ /* spawn energy getting too low */
-                    creep.memory.passbit = 0;
-                    
+            if (extension){ // FOUND
+                var process = creep.transfer(extension, RESOURCE_ENERGY);
+                
+                if (process == OK){
+                    break; 
                 }
                 
-            }else{ /* search for extensions */
-            creep.memory.passbit = 0;
-            upgradetarget = Game.getObjectById(creep.memory.upgradetarget); /* cleaning wait bits */
-            if (upgradetarget){upgradetarget.memory.waitbit = 0
-            creep.memory.upgradetarget = 0;}
+                if (process == ERR_NOT_IN_RANGE){
+                    creep.moveTo(extension);
+                }
+                
+                
+                if (process == ERR_FULL){
+                    creep.memory.job = creep.memory.job + 1;
+                    break; 
+                }
+                
             }
-		} 
-		if( process == ERR_FULL && transferYeri != Game.spawns.Spawn1) {
-            /* creep.memory.passbit = 1; */
-            console.log("Carrier: Extension became full.");
-		}
-		if( transferYeri == null && creep.memory.passbit != 2){
-		    creep.memory.passbit = 1;
-		    /* console.log("Harvester: All extensions are full."); */
-		}
-		if( creep.memory.chkcount > 9 && creep.memory.passbit != 2){ /* periodically check for extensions */
-		    creep.memory.passbit = 0;
-		    creep.memory.chkcount = 1;
-		}	    
+            // NOT FOUND
+            
+            if (extension == null){ // all extensions are full
+                creep.memory.job = creep.memory.job + 1;
+            }
+        
+            break;
+            
+        case 2: // SPAWN
+            var spawn = creep.room.find(FIND_MY_SPAWNS)[0];
+            if (spawn.energy < spawn.energyCapacity){
+                var process = creep.transfer(spawn, RESOURCE_ENERGY);
 
-}
+                if (process == OK){
+                    break; 
+                }
+                
+                if (process == ERR_NOT_IN_RANGE){
+                    creep.moveTo(spawn);
+                }
+
+                if (process == ERR_FULL){
+                    creep.memory.job = creep.memory.job + 1;
+                    break; 
+                }
+            }else{
+                    creep.memory.job = creep.memory.job + 1;
+                    break;              
+            }
+            break;
+        case 3: // TOWER
+            var tower = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: function (s){
+                return s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity/4*3; // UNDER %75
+            }
+            });
+            if (tower){ // FOUND
+                var process = creep.transfer(tower, RESOURCE_ENERGY);
+                
+                if (process == OK){
+                    break;
+                }
+                
+                if (process == ERR_NOT_IN_RANGE){
+                    creep.moveTo(tower);
+                }
+                
+                
+                if (process == ERR_FULL){
+                    creep.memory.job = creep.memory.job + 1;
+                    break; 
+                }
+            }     
+            // NOT FOUND
+
+            if (tower == null){ // all towers are full
+                creep.memory.job = creep.memory.job + 1;
+            }           
+            break;
+            
+        case 4: // BUILDERS //TODO IMPLEMENT TIMEOUT
+            var upgrader = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
+            filter: function(object) {
+            return object.memory.role == "builder" && object.memory.upgrademode == 1 && object.carry.energy < object.carryCapacity/100*80;
+            }
+            });
+            
+            if (upgrader){ // FOUND
+                if (creep.transfer(upgrader, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(upgrader);
+                }                
+            }
+            // NOT FOUND
+            
+            if (upgrader == null){
+               creep.memory.job = creep.memory.job + 1; 
+            }
+
+            break;
+
+        case 5: // STORAGE
+            if(creep.room.storage){
+                if (creep.transfer(creep.room.storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(creep.room.storage);
+                }
+                
+            }else{
+              creep.memory.job = creep.memory.job + 1;   
+            }
+        
+            break;
+            
+        case 6: // LINK
+        
+            if (creep.room.storage.store.energy < 15000){ console.log("carrier: less than 15000 energy, skipping job 6"); creep.memory.job = creep.memory.job + 1; break; }
+            var linkpoints = creep.room.memory.linkpoints;
+            if (typeof linkpoints !== "undefined"){ linkFrom = creep.room.lookForAt('structure', linkpoints[0][0], linkpoints[0][1])[0]; }
+            if (linkFrom && linkFrom.energy != linkFrom.energyCapacity){
+                if (creep.transfer(linkFrom, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(linkFrom);
+                }                
+            }else{
+              creep.memory.job = creep.memory.job + 1;   
+            }
+        
+            break;
+        
+    }
+      
 }
