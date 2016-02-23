4c51ebc95c76e4686af7111ac0c2804c81f64092
Aranel Surion <aranel@aranelsurion.org>
2016-02-23 06:51:19 +0200

    Miners can now be ordered to prefer a resource by flags and it can drop energy to adjacent storage

--- miner.js
+++ miner.js
@@ -8,18 +8,30 @@
  
  module.exports = function (creep) {
 
-        /* JOB: Mining */
+        /* JOB: Mining - IF FLAG EXISTS */
+        var target = eval("Game.flags." + creep.memory.target);
+        var cstorage = creep.memory.cstorage;
+        
+
+        
+        if (target){ target = target.pos.lookFor('source');
+        if (creep.harvest(target[0]) == ERR_NOT_IN_RANGE || -6){
+                creep.moveTo(target[0]);
+        }}
+		
+		/* JOB: Mining - IF FLAG DOES NOT EXIST */
+		if (target == null){
 		var sources = creep.room.find(FIND_SOURCES);
 		if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE || -6) {
 			creep.moveTo(sources[0]);
-		}
+		}}		
 		
-		//If STRUCTURE_STORAGE exists. Place it adjacent to miners for most efficiency.
-		if(creep.room.storage && creep.carry.energy > 40){
+        if (cstorage == 1 && creep.carry.energy > 40){
 		    if (creep.transfer(creep.room.storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
 		        creep.drop(RESOURCE_ENERGY); // drop energy if you can't store, to be picked up by adjacent miner.
-		    }
-		}
+		    }            
+        }
+
 		
 		// If not, energy is dropped. Pickup as much as you can. */
         if(creep.pos.findClosestByRange(FIND_DROPPED_ENERGY)){
